7f3e37f5146eb4e2e971d842c07e2a16354d7635
KusumaPriyaRaavi <84385561+KusumaPriyaRaavi@users.noreply.github.com>
2021-06-26 18:52:24 +0530

    Files Added

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>ReactJS BootCamp</h1>
+      <p>A basic Web Dev ReactJS BootCamp</p>
+    </div>
+  );
+}
+
+export default Info;
